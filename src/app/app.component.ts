f27526970a37a6c8578a0fc94b1e8634f060dfdb
Camille DINET <[camidinet@gmail.com](mailto:camidinet@gmail.com)>
2023-07-18 08:41:56 +0200

    add progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs';
 
 
 @Component({
@@ -98,9 +97,10 @@ deleteMusic(i:any){
 updateProgressBar(event: Event) {
   const audio = event.target as HTMLAudioElement;
   this.progress = (audio.currentTime / audio.duration) * 100;
+  
   if(this.progress >= 100 && (this.readingTitle===this.filesArray.length - 1)) {
     this.readingTitle=0;
-    this.playMusic() 
+    this.playMusic();
   } else if(this.progress >= 100 && this.readingTitle!==(this.filesArray.length - 1)){  
     this.readingTitle++;
     this.playMusic();   
@@ -110,5 +110,14 @@ updateProgressBar(event: Event) {
 interceptPath(e:any){
   let file = e.target.files[0];
   this.blobLocalMusic = URL.createObjectURL(file);
+}
+selectDurating(event:any){
+  this.break();
+   console.log(event.target.getBoundingClientRect().x);//début
+   console.log(event.target.getBoundingClientRect().x+250);//fin
+   console.log(event.clientX);//click
+   this.myAudioElement.nativeElement.currentTime = event.clientX-event.target.getBoundingClientRect().x;
+   this.playMusic();
+    
 }
 }
